fixed bug in formulaUtils master formula generation

formulaUtils.py
@@ -192,7 +192,7 @@ def findBestForm(mass, parentForm, toleranceDa = 0.005, charge = 0, verbose = Fa
 def generateAllForms(parentForm):
     l1 = [[atom for x in range(atomNum)] for atom, atomNum, _, _ in molmass.Formula(parentForm).composition()]
     l2 = list(np.concatenate(l1). flat)
-    mForms = (molmass.Formula("".join(x)) for l in range(1, len(l2)) for x in more_itertools.distinct_combinations(l2, l))
+    mForms = (molmass.Formula("".join(x)) for l in range(1, len(l2) + 1) for x in more_itertools.distinct_combinations(l2, l))
     outList = [(m.formula, m.isotope.mass) for m in mForms]
     return(outList)
 